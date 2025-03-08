7ab1240b32a7bc8d789ea0d862ddbdd40fa542dc
Amir <AmirIqbal1@users.noreply.github.com>
2025-03-08 19:58:59 +0000

    Update update.sh
    added sudo for automation with desktop shortcut

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ echo "Starting system update..."
 
 # Reconfigure dpkg
 echo "Reconfiguring dpkg..."
-dpkg --configure -a
+sudo dpkg --configure -a
 if [ $? -ne 0 ]; then
   echo "dpkg reconfiguration failed."
   exit 1
@@ -12,7 +12,7 @@ fi
 
 # Update package lists
 echo "Updating package lists..."
-apt update
+sudo apt update
 if [ $? -ne 0 ]; then
   echo "apt update failed."
   exit 1
@@ -20,23 +20,23 @@ fi
 
 # Upgrade packages
 echo "Upgrading packages..."
-apt upgrade -y
+sudo apt upgrade -y
 if [ $? -ne 0 ]; then
   echo "apt upgrade failed."
   exit 1
 fi
 
-# comment the following lines to stop update of Flatpak packages
- echo "Updating Flatpak packages..."
- flatpak update -y
- if [ $? -ne 0 ]; then
-   echo "Flatpak update failed."
-   exit 1
- fi
+# Update Flatpak packages (comment to disable)
+echo "Updating Flatpak packages..."
+sudo flatpak update -y
+if [ $? -ne 0 ]; then
+  echo "Flatpak update failed."
+  exit 1
+fi
 
 # Fix broken dependencies
 echo "Fixing broken dependencies..."
-apt install -f
+sudo apt install -f
 if [ $? -ne 0 ]; then
   echo "Fixing broken dependencies failed."
   exit 1
@@ -44,34 +44,32 @@ fi
 
 # Clean up
 echo "Cleaning up unnecessary packages and files..."
-apt clean
-apt autoclean
-apt autoremove -y
+sudo apt clean
+sudo apt autoclean
+sudo apt autoremove -y
 if [ $? -ne 0 ]; then
   echo "Clean up failed."
   exit 1
 fi
 
-# Uncomment for Auto reboot if needed
+# Auto reboot check
 echo "=================================================="
 if [ -f /var/run/reboot-required ]; then
-  echo " - Reboot required. "
-#  reboot
+  echo " - Reboot required."
+#  sudo reboot
 else
   echo "System update completed successfully. No reboot required."
 fi
 echo "=================================================="
 
-#monitors last login attempts
+# Monitor failed login attempts
 echo "Monitoring failed login attempts..."
-touch /var/log/btmp
-chmod 600 /var/log/btmp
+sudo touch /var/log/btmp
+sudo chmod 600 /var/log/btmp
 echo "=================================================="
-#lets excute command to check
 lastb
 echo "=================================================="
 
-echo""
 # End of the script
 echo "Script finished."
-echo""
+echo ""
